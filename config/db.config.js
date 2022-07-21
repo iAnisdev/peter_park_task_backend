86998cafb5a22655a5fe388ee3dfc80af67b72ab
marahmanjs <imarahmandev@gmail.com>
2022-07-21 21:02:42 +0500

    controller setup

--- config/db.config.js
+++ config/db.config.js
@@ -1,13 +1,12 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "password",
-    DB: "testdb",
-    dialect: "mysql",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  };
+  HOST: "localhost",
+  USER: "root",
+  DB: "permits",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
--- routes/permits.js
+++ routes/permits.js
@@ -1,19 +1,25 @@
 var express = require('express');
+const { body } = require('express-validator');
+const Permit = require('../controllers/permits.controller.js');
 var router = express.Router();
 // Create a new permit
-router.post("/", tutorials.create);
+router.post("/", 
+body('country').exists().withMessage('Country Required for permit'),
+body('licence').exists().withMessage('Licence Required for permit'),
+body('startDate').exists().withMessage('Start Date Required for permit'),
+body('endDate').exists().withMessage('End Date Required for permit'), Permit.create);
 
 // Retrieve all permit
-router.get("/", tutorials.findAll);
+router.get("/", Permit.findAll);
 
 // Retrieve a single permit with id
-router.get("/:id", tutorials.findOne);
+router.get("/:id", Permit.findOne);
 
 // Update a permit with id
-router.put("/:id", tutorials.update);
+router.put("/:id", Permit.update);
 
 // Delete a permit with id
-router.delete("/:id", tutorials.delete);
+router.delete("/:id", Permit.delete);
 
 
 module.exports = router;
